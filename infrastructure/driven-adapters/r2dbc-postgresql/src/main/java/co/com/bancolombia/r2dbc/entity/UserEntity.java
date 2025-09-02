413a79737a52ebb14e2a9af09f333d850c7f1e0b
Andrea Timaran <andreaestefania12@hotmail.com>
2025-09-02 17:46:09 -0500

    Update @Column annotations in UserEntity for correct column mapping

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/entity/UserEntity.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/entity/UserEntity.java
@@ -3,6 +3,7 @@ package co.com.bancolombia.r2dbc.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
@@ -12,11 +13,25 @@ import java.math.BigDecimal;
 public class UserEntity {
     @Id
     private Long id;
+
+    @Column("first_name")
     private String firstName;
+
+    @Column("last_name")
     private String lastName;
+
+    @Column("email")
     private String email;
+
+    @Column("identification")
     private String identification;
+
+    @Column("phone_number")
     private String phoneNumber;
+
+    @Column("base_salary")
     private BigDecimal baseSalary;
+
+    @Column("role_id")
     private Long idRole;
 }
